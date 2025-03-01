e9fd93cc2413fd99b6b3b17c67caf3590938f31b
mths0303s <matheusaraujo1460@gmail.com>
2025-03-01 01:09:16 -0300

    Adding files via upload

--- pages/index.php
+++ pages/index.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a517964f1a.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/styleblog.css">
-    <title>Blog Gospel</title>
+    <link rel="stylesheet" href="../styles/stylehome.css">
+    <title>Home</title>
 </head>
 <body>
-    <header>
+<header>
         <div class="topo">
             <div class="logo"><i class="fa-solid fa-book-open"></i> <span>Holly</span></div>
             <div class="container">
                 <div class="menu">
                     <nav>
                         <a href="home.php">Home</a>
-                        <a href="#">Blog</a>
+                        <a href="blog.php">Blog</a>
                         <a href="#">Sobre</a>
                         <a href="#">Contato</a>
                     </nav>
@@ -28,49 +29,24 @@
             </div>
         </div>
     </header>
+    <div class="principal">
+        <h1>Seja Bem Vindo</h1>
+        <h3>Junte-se a nós nessa experiência!</h3>
+    </div>
 
-    <main>
-        <h1>
-            Parábola dos talentos
-        </h1>
-
-        <div class="img-top">
-            <img src="img\img_principal.webp" alt="">
+    <div class="blocoTextos">
+        <div class="bloco">
+            <h2>Quem somos</h2>
+            <p>Somos uma igreja evangélica que prega a palavra de Deus, com o objetivo de levar a palavra de Deus a todos os povos, para que todos possam conhecer a verdade e serem libertos.</p>
         </div>
-
-        <h2>
-            Mateus Cap. 25 | NVI
-        </h2>
-
-        <p>
-            14 “E também será como um homem que, ao sair de viagem, chamou seus servos e confiou-lhes os seus bens.<br>15 A um deu cinco talentos , a outro dois, e a outro um; a cada um de acordo com a sua capacidade. Em seguida partiu de viagem.<br>16 O que havia recebido cinco talentos saiu imediatamente, aplicou-os, e ganhou mais cinco.<br>17 Também o que tinha dois talentos ganhou mais dois.<br>18 Mas o que tinha recebido um talento saiu, cavou um buraco no chão e escondeu o dinheiro do seu senhor.<br>19 “Depois de muito tempo o senhor daqueles servos voltou e acertou contas com eles.<br>20 O que tinha recebido cinco talentos trouxe os outros cinco e disse: O senhor me confiou cinco talentos; veja, eu ganhei mais cinco.<br>21 “O senhor respondeu: Muito bem, servo bom e fiel! Você foi fiel no pouco, eu o porei sobre o muito. Venha e participe da alegria do seu senhor!<br>22 “Veio também o que tinha recebido dois talentos e disse: O senhor me confiou dois talentos; veja, eu ganhei mais dois.<br>23 “O senhor respondeu: Muito bem, servo bom e fiel! Você foi fiel no pouco, eu o porei sobre o muito. Venha e participe da alegria do seu senhor!<br>24 “Por fim, veio o que tinha recebido um talento e disse: Eu sabia que o senhor é um homem severo, que colhe onde não plantou e junta onde não semeou.<br>25 Por isso, tive medo, saí e escondi o seu talento no chão. Veja, aqui está o que pertence ao senhor.<br>26 “O senhor respondeu: Servo mau e negligente! Você sabia que eu colho onde não plantei e junto onde não semeei?<br>27 Então você devia ter confiado o meu dinheiro aos banqueiros, para que, quando eu voltasse, o recebesse de volta com juros.<br>28 “Tirem o talento dele e entreguem-no ao que tem dez.<br>29 Pois a quem tem, mais será dado, e terá em grande quantidade. Mas a quem não tem, até o que tem lhe será tirado.<br>30 E lancem fora o servo inútil, nas trevas, onde haverá choro e ranger de dentes.
-        </p>
-
-        <h2>
-            Reflexões
-        </h2>
-
-        <img class="img-top" src="img/reflexao.jpeg" alt="">
-
-        <p>- Deus entregou a cada pessoa de acordo com a sua capacidade, tudo oque está em suas mãos o próprio Deus permitiu, porque Ele viu que você é capaz, então tenha bom animo assim como foi dito a Josué, pois Deus te deu esse dom ou qualquer coisa que Ele possa ter deixado em suas mãos porque sabe que você é capaz de aguentar, capaz de frutificar com isso, capaz de realizar maravilhas e milagres, pense no que Ele deixou nas suas mãos e comece o quanto antes a utilizar na obra d’Ele!<br></p>
-
-        <p>Josué 1:9<br>
-
-        Não fui eu que ordenei a você? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor , o seu Deus, estará com você por onde você andar.<br></p>
-
-        <p>- Temos o dever de multiplicar aquilo que Deus deixou em nossas mãos, temos que cumprir o ide do Senhor a todo custo, não enterrar nossos dons. Pois Deus vai requerer isso de nos quando Ele vier nos buscar. Então reflita: você está granjeando oque Deus te deu, para que Ele possa colher onde não plantou, como está escrito na parábola? Se não, comece a faze-lo o quanto antes, comece dizer e a cumprir com fevor aquilo que Isaías disse na passagem:<br>
-
-        <p>Isaías 6:8<br>
-
-        Então, ouvi a voz do Senhor, conclamando: ― A quem enviarei? Quem irá por nós? Eu respondi: ― Aqui estou. Envia‑me!<br></p>
-
-        <p>- O servo que via seu senhor como um homem severo, só experimentou a severidade dele. Como você tem visto o Senhor? Devemos ver o Senhor como nosso amigo e fazer tudo para que a obra que Ele começou cresça, não por medo dele, Ele não quer que tenhamos medo, quer que tenhamos intimidade e sejamos verdadeiramente seus amigos.<br></p>
-
-        <p>João 15:15 <br>
-        Já não os chamo servos, porque o servo não sabe o que faz o senhor. Em vez disso, eu os tenho chamado amigos, porque tudo o que ouvi do meu Pai tornei conhecido a vocês.</p>
-
-
-        <h2>Deus é bom!</h2>
-    </main>
+        <div class="bloco">
+            <h2>Missão</h2>
+            <p>Nossa missão é levar a palavra de Deus a todos os povos, para que todos possam conhecer a verdade e serem libertos.</p>
+        </div>
+        <div class="bloco">
+            <h2>Visão</h2>
+            <p>Nossa visão é ser uma igreja que prega a palavra de Deus, com o objetivo de levar a palavra de Deus a todos os povos, para que todos possam conhecer a verdade e serem libertos.</p>
+        </div>
+    </div>
 </body>
 </html>
